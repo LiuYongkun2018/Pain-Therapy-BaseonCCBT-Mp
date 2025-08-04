afb89d2def08dbf465f18590d53474abf331fb11
ykliu6 <ykliu6@iflytek.com>
2025-08-04 17:00:38 +0800

    开发顶部导航栏

--- components/navigation-bar/navigation-bar.js
+++ components/navigation-bar/navigation-bar.js
@@ -97,6 +97,12 @@ Component({
         })
       }
       this.triggerEvent('back', { delta: data.delta }, {})
+    },
+    home() {
+      wx.switchTab({
+        url: '/pages/index/index'
+      })
+      this.triggerEvent('home', {}, {})
     }
   },
 })
